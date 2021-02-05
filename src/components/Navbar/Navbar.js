e2f85dbecfaae887fcef1fcf17ad3db3a9446823
mariaprang <marie.prangishvili@gmail.com>
2021-02-06 02:25:34 +0400

    Finilized the navbar frontend. 1) Added the background photo cover 2) Added the hooks to functional component to add active class on scroll

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Route, NavLink, HashRouter } from "react-router-dom";
 // import { Hello, Profile, Resume, Portfolio, Contact } from "../../components";
@@ -10,25 +10,74 @@ import "./Navbar.css";
 //   background-color: black;
 // `;
 
-const Navbar = () => (
-  <div className="navbar">
-    <div className="navbar-menu">
-      <ul className="header">
-        <li>
-          <NavLink to="/">hello</NavLink>
-        </li>
-        <li>
-          <NavLink to="/resume">Resume</NavLink>
-        </li>
-        <li>
-          <NavLink to="/portfolio">Portfolio</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
+const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 20) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeBackground);
+
+  return (
+    <nav className={navbar ? "navbar active" : "navbar"}>
+      <div className="navbar-menu">
+        <ul className="header">
+          <li>
+            <NavLink className={navbar ? "nav-dark" : "nav-light"} to="/">
+              hello
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={navbar ? "nav-dark" : "nav-light"} to="/resume">
+              Resume
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={navbar ? "nav-dark" : "nav-light"}
+              to="/portfolio"
+            >
+              Portfolio
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={navbar ? "nav-dark" : "nav-light"}
+              to="/contact"
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+// const Navbar = () => (
+//   <nav className="navbar active">
+//     <div className="navbar-menu">
+//       <ul className="header">
+//         <li>
+//           <NavLink to="/">hello</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/resume">Resume</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/portfolio">Portfolio</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/contact">Contact</NavLink>
+//         </li>
+//       </ul>
+//     </div>
+//   </nav>
+// );
 
 export default Navbar;
