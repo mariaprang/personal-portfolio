1bf2f5bfdac5b23e37b714b5f09a2bcc1b869848
mariaprang <marie.prangishvili@gmail.com>
2021-02-06 15:46:25 +0400

    Added front-end changes to the main page and contact footer

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Typical from "react-typical";
+import "./Contact.css";
 
 const Contact = () => (
   <div>
-    <div className="skills-wrap section-box">
-      <h1 className="headline-text">
+    <div className="skills-wrap contact-wrap">
+      <h1 className="headline-text contact-text">
         <Typical steps={["Get in touch_", 1000]} loop={1} wrapper="p" />
       </h1>
     </div>
