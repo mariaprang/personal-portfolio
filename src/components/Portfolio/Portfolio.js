b2d35c9fe7780a301b7ea04fffdbbbbd59aa6ad8
mariaprang <marie.prangishvili@gmail.com>
2021-03-31 04:39:25 +0400

    added some info into the 'porftolio' section

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Portfolio.css";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Typical from "react-typical";
 
@@ -20,12 +21,9 @@ function Portfolio() {
         <div className="project-description">
           <div className="project-text">Jira-like software</div>
           <div className="description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proide
+            Inspired by an exisiting "Jira" software,
+          this project implements <b>some use cases</b> required for a project management system. 
+          It enables company members to register, login, create new project boards, new tickets, and add members to the task and to the team.
             <div className="tools-wrap">
               <div className="lang-wrap">HTML</div>
               <div className="lang-wrap">CSS</div>
@@ -62,12 +60,10 @@ function Portfolio() {
           <div className="project-description">
             <div className="project-text">Java Tutor at Preply.com</div>
             <div className="description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proide
+              I've been a tutor at an online teaching platform 'Preply.com' since 2016. To this day I have had an opportunity
+              to teach Core Java, SpringBoot, JavaScript, HTML, CSS, MySQL. I've helped my students build their portfolio projects and
+              prepare for interviews. I'm proud to have tought a bit over 3200 hours on the platform and have gained 40+ positive reviews.
+              You're welcome to click the link below and view my profile page.
               <div className="tools-wrap">
                 <div className="lang-wrap">Web Dev</div>
                 <div className="lang-wrap">Java</div>
@@ -77,15 +73,52 @@ function Portfolio() {
                 <div className="lang-wrap">Mentoring</div>
               </div>
               <div className="source-wrap row">
-                <a href="https://github.com/mariaprang/project-management-saasovation">
+                <a href="https://preply.com/en/tutor/66210/" target="_blank">
+                  <FontAwesomeIcon
+                    icon={faLink}
+                    className="icons"
+                    target="_blank"
+                  />
+                </a>
+                <a className="web-link" href="https://preply.com/en/tutor/66210/" target="_blank">
+                  www.preply.com
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="project-wrap">
+        <div className="row">
+          <div className="image-wrap">
+            <img
+              className="project-image"
+              src="images/fin-address.png"
+            ></img>
+          </div>
+          <div className="project-description">
+            <div className="project-text">Created web templates on Fiverr.com and Upwork.com</div>
+            <div className="description">
+            I have worked as a freelance web developer and developed modern and mobile responsive websites using HTML5, CSS3 and JavaScript. 
+            
+              <div className="tools-wrap">
+                <div className="lang-wrap">Web Dev</div>
+                <div className="lang-wrap">HTML5</div>
+                <div className="lang-wrap">CSS3</div>
+                <div className="lang-wrap">JavaScript</div>
+                <div className="lang-wrap">Responsive Web Design</div>
+              </div>
+              <div className="source-wrap row">
+                <a href="https://github.com/mariaprang/finaddress-m8werk" target="_blank">
                   <FontAwesomeIcon
                     icon={faGithub}
                     className="icons"
                     target="_blank"
                   />
                 </a>
-                <a className="web-link" href="www.example.com">
-                  www.example.com
+                <a className="web-link" href="https://objective-franklin-7af242.netlify.app/">
+                  www.template-example.com
                 </a>
               </div>
             </div>
